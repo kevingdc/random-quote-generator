b43990266a07bbb770019b16850a4b983074f2a9
Kevin Chan <kevin.gray.chan@gmail.com>
2021-03-28 14:06:47 +0800

    chore: Adjust folder structure

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <p className="text-6xl">Hello World</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
